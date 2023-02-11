319ddd4d87ceef00e200f0cd988ad7af2dcc4791
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-02-11 15:16:20 +0500

    Variables in Python

---
+++ Day_2_Variables.py
@@ -0,0 +1,7 @@
+# Day 2 Veriables ..............................................................................................
+
+# price = 10 # int
+# price = 2.32 # float
+# name = "haris" #char
+# is_pulished = False #boolean
+# print (price)
