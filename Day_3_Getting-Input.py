1a9a0f4f50c8c0d921728818678372c19b1a034a
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-02-12 10:51:02 +0500

    Add files via upload

---
+++ Day_3_Getting-Input.py
@@ -0,0 +1,10 @@
+# Day 3 Getting Input ..........................................................................................
+
+# name  = input ("What is Your Name?\n")
+# print ("Hi", name)
+
+# EXERCISE.2: Ask two questions: persos name. Then Print a msg like "haris like red"
+
+# name = input("What is your name?\n ")
+# color = input("What is your favourite color?\n ")
+# print(name , "like", color)
