621502fc975b5bdbdf2a759ba39d5976d999cec0
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-02-12 10:49:07 +0500

    Added Exercise

--- Day_1_Print_Statment.py
+++ Day_1_Print_Statment.py
@@ -1,6 +1,13 @@
-# Day 1 Print Statment .........................................................................................
+# Day 2 Veriables ..............................................................................................
 
-# print ("Hello world")
-# print ("0----")
-# print (" !!!!")
-# print ("*" * 10)
+# price = 10
+# price = 20 # int
+# name = "haris" #char
+# is_pulished = False #boolean
+# print (price)
+
+# EXERCISE.1: We check in a patient named Hris. He is 20 year old nd new patient.
+
+# p_Name = "Haris"
+# P_Age = 20
+# P_new = True
