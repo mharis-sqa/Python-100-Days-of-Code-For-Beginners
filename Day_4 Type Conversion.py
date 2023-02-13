70c7b375428aea127cb203d2670491ebbe3efdef
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-02-13 16:23:13 +0500

    Add files via upload

---
+++ Day_4 Type Conversion.py
@@ -0,0 +1,13 @@
+# Day 4 Type Conversion ........................................................................................
+
+# birth_year = input("Birth year: ")
+# print(type(birth_year))
+# age = 2023 - (int(birth_year))
+# print(type(age))
+# print(age)
+
+# EXERCISE.3: Ask a user their weight in (pounds) and convert it into kilograms
+
+# pound = input("Your weight in pounds: ")
+# kg = int(pound) * 0.4536
+# print("Your weight is ", kg ,"kg")
