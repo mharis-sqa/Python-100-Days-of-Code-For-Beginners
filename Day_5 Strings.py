d1dcf57a28ffccebf40f3b3895bcbc2d8ce2e35a
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-02-14 14:11:14 +0500

    Add files via upload

---
+++ Day_5 Strings.py
@@ -0,0 +1,17 @@
+# Day 5 strings .................................................................................................
+
+# course_1 = "1. Python for Beginners\n"
+# course_2 = "2. Python for Beginner's\n"
+# course_3 = '3. Python for "Beginners"\n'
+# course_4 = """4. This course is for Python's "biggner" fans."""
+# print(course_1 , course_2, course_3, course_4)
+
+# course = "Python is goo language"
+# print(course[0])
+# print(course[3])
+# print(course[-2])
+# print(course[0:3])
+# print(course[1:])
+# print(course[:5])
+# print(course[1:0])
+# print(course[:]) 
