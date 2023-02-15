82253269654e99da98f9af90c760aa4382e2e94c
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-02-15 10:44:53 +0500

    Formatted Strings

---
+++ Day_6 Formated Strings.py
@@ -0,0 +1,8 @@
+# Day 6 Formated Strings ........................................................................................
+
+# first = "Muhammad"
+# last = "Haris"
+# message = first  + ' [' + last + '] is a coder'
+# msg = f'{first} [{last}] is a coder'
+# print(message)
+# print(msg)
