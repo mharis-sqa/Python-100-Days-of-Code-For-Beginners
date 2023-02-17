9d3e2ab4eb6d931eb7a68a6069f06908e334b569
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-02-17 10:35:17 +0500

    Add files via upload

---
+++ Day_7 String Methods.py
@@ -0,0 +1,11 @@
+ # Day 7 String Methods ..........................................................................................
+
+# course = "Python is king language"
+# print(course)
+# print(len(course))
+# print (course.upper())
+# print (course.lower())
+# print (course.title())
+# print(course.find('i'))
+# print(course.replace('king', 'good'))
+# print('python' in course)
