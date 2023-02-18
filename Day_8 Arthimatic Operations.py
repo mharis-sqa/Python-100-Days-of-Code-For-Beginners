5c251ae75cff2966bad7c8098b3bfc225be674aa
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-02-18 21:08:32 +0500

    Athematic Operations

---
+++ Day_8 Arthimatic Operations.py
@@ -0,0 +1,13 @@
+# Day 8 Arthimatic Operations ....................................................................................
+
+# print(2+2) # Plus
+# print(2-2) # Minus
+# print(2*2) # Multiply
+# print(2/2) # Divide
+# print(2//2) # Round off Divide
+# print(2%2) # Reminder
+# print(2**2) # Exponention / 10 Power
+# x= 3
+# # x = x + 3 
+# x+= 3 # Assingment operater
+# print(x)
