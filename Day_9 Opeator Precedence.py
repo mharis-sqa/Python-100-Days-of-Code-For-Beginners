3a5889794426d7af5180b7f4c0e00a2d48457942
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-02-19 10:10:02 +0500

    Opeator Precedence

---
+++ Day_9 Opeator Precedence.py
@@ -0,0 +1,28 @@
+# Day 9 Opeator Precedence .......................................................................................
+
+# x = 10 + 3 * 2 **2   
+# print(x)
+                        # Applying BODMAS Rule
+# x = (2+3) * 10 - 3
+# print(x)
+
+
+# EXERCISE:
+
+# a = 20
+# b = 10
+# c = 15
+# d = 5
+# e = 0
+
+# e = (a + b) * c / d       #( 30 * 15 ) / 5
+# print "Value of (a + b) * c / d is ",  e
+
+# e = ((a + b) * c) / d     # (30 * 15 ) / 5
+# print "Value of ((a + b) * c) / d is ",  e
+
+# e = (a + b) * (c / d);    # (30) * (15/5)
+# print "Value of (a + b) * (c / d) is ",  e
+
+# e = a + (b * c) / d;      #  20 + (150/5)
+# print "Value of a + (b * c) / d is ",  e
