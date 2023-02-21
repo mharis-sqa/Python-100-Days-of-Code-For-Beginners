e91fd2357ddb4f8128b32150c56b061d36b23c30
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-02-21 21:24:31 +0500

    IF & Else statements

---
+++ Day_11 If Else Statments.py
@@ -0,0 +1,29 @@
+# Day 11 If Else Statments .........................................................................................
+
+# its_hot = False
+# its_cold = False
+
+# if its_hot:
+#     print("Its a hot day")
+#     print("Drink plenty of water")
+
+# elif its_cold:
+#     print("Its Cold Day")
+#     print("Wear warm clothes")
+
+# else:
+#     print("Its a lovely Day")
+
+# EXERCISE: Calculate Dow Payments of House / if buyer credit is good or not
+
+# buyer_C_G = False
+# price = 1000000
+
+# print("Price of House is: ", price)
+
+# if buyer_C_G:
+#     down_payment = 0.1 * price
+# else:
+#     down_payment = 0.2 * price
+
+# print(f"Down payment is: ${down_payment}")
