3e9d94ac27ab947fef5322b905a1450831a4243b
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-02-22 18:58:21 +0500

    Add files via upload

---
+++ Day_12 Logical Operators.py
@@ -0,0 +1,23 @@
+# Day 12 Logical Operators .........................................................................................
+
+# high_income = False
+# good_credit = False
+
+
+# if high_income or good_credit:          # and or
+#     print("You are eligable for loan")
+
+# else:
+#     print("You are not eligable for loan")
+
+# EXERCISE
+
+# good_credit = True
+
+# has_criminal_record = True
+
+# if  good_credit and not has_criminal_record:          # and not
+#     print("You are eligable for loan")
+
+# else:
+#     print("You are not eligable for loan")
