b557d6552c7d75880066efacf8a0aa6d2a526dbb
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-02-23 20:03:34 +0500

    Comparison Operators

---
+++ Day_13 Comparison Operators.py
@@ -0,0 +1,38 @@
+# Day 13 Comparison Operators .........................................................................................
+
+
+# tmperature = int(input("Enter the temprature: "))
+
+# if tmperature >= 30:
+#     print("Its hot today")
+
+# elif tmperature <= 10:
+#     print("Its cold today")
+
+# else:
+#     print("Today is a good day")
+
+
+# EXERCISE:
+
+# name = input("Enter Your Name: ")
+
+# if len(name) < 3:
+#     print("Name must be atleast 4 chrecters")
+# elif len(name) > 15:
+#     print("Name is too long")
+# else:
+#     print("Your name is: ", name)
+
+# PROJECCT: WEIGHT CONVERTER:
+
+# weight = int(input("Enter Your Weight: "))
+
+# unit = input("(L)bs or (K)g: ")
+
+# if unit.upper() == "L":
+#     print("Your Weight is: ", weight*0.45 ,"Kg")
+# elif unit.upper() == "K":
+#     print("Your Weight is: ", weight*2.2, "Lbs")
+# else:
+#     print("Enter right information")
