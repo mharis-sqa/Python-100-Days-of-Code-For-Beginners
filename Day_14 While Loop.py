8aeed2fa2ce5deae11bc6062c9bdd541e2fffb28
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-02-24 21:18:18 +0500

    While Loop

---
+++ Day_14 While Loop.py
@@ -0,0 +1,22 @@
+# Day 14 While Loop .........................................................................................
+
+# i = 1
+# while i <= 5:
+#     print('*' * i)
+#     i = i=i+1
+# print("Done")
+
+# PROJECT: Guessing Game
+
+# number = 9
+# Guess_Count = 0
+# Guess_Limit = 3
+
+# while Guess_Count < Guess_Limit:
+#     Guess = int(input("Guess the number: "))
+#     Guess_Count += 1
+#     if Guess == number:
+#         print("You Win")
+#         break
+# else:
+#     print("Game Over")
