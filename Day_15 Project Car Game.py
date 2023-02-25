5f5c980a6a9c3021ba0ea9884dcd0c6e4ec177e4
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-02-25 20:40:23 +0500

    Project Car Game

---
+++ Day_15 Project Car Game.py
@@ -0,0 +1,29 @@
+# Day_15 Project Car Game .........................................................................................
+
+# command = ""
+# started = False
+
+# while True:
+#     command = input("> ").lower()
+#     if command == "start":
+#         if started:
+#             print("Car is already started")
+#         else:
+#             started = True
+#         print("Car started")
+#     elif command == "stop":
+#         if not started:
+#             print("Car is already stoped")
+#         else:
+#             started = False
+#         print("Car stoped")
+#     elif command == "help":
+#         print("""
+#     1. start to start the car
+#     2. stop to stop the car 
+#     3. help for help
+#                 """)
+#     elif command == "quit":
+#         break
+# else:
+#     print("Sorry Wrong Command")
