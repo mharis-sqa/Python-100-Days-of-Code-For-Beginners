97ed76ec2dbff6c6b6e5c00209b609e17b017eda
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-03-02 12:04:46 +0500

    For Loops

---
+++ Day_16 For Loop.py
@@ -0,0 +1,22 @@
+# Day_16 For Loop .........................................................................................
+
+# for iter in 'Python':
+#     print(iter)
+
+# for iter in ['Haris', 'Shakir']:
+#     print(iter)
+
+# for iter in ['1', '2', '3']:
+#     print(iter)
+
+# for iter in range(10):
+#     print(iter)
+
+# EXERCISE Add all the prices of items in the shoppig cart
+
+# prices = [18,22,30]
+
+# total = 0
+# for price in prices:
+#     total += price
+# print(f"Total = {total}")
