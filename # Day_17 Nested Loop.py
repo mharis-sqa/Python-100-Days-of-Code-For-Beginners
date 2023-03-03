33fc8ee7fa34ed4941ea21350c3e2c4c60cb7b4a
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-03-03 19:03:22 +0500

    Nested Loops

---
+++ # Day_17 Nested Loop.py
@@ -0,0 +1,32 @@
+# Day_17 Nested Loop .........................................................................................
+
+# for x in range(4):
+#     for y in range(3):
+#         print(f'({x},{y})')
+
+
+# EXERCISE Print F with stars usiing loops
+
+# Cheating code::
+# stars = [5, 2, 5, 2, 2]
+
+# for x_count in stars:
+#     print("*" * x_count)
+
+# Actuall soloution:
+# stars = [5, 2, 5, 2, 2]
+
+# for x_count in stars:
+#     output = ""
+#     for count in range(x_count):
+#         output += "*"
+#     print(output)
+
+# # Now Print L with x usig nested loops
+# x = [1,1,1,1,1,1,8]
+
+# for x_count in x:
+#     output = ""
+#     for count in range(x_count):
+#         output += "x"
+#     print(output)
