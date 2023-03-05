5085f260a1d44d76dd0ddfe6aaf92dd8a83f9eba
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-03-05 09:20:04 +0500

    Lists

---
+++ Day_ 18 Lists.py
@@ -0,0 +1,20 @@
+# Day_ 18 Lists in Python .........................................................................................
+
+# names = ["Haris", "Shakir", "Anum", "Shiza", "Jibran"]
+# print(names[1])
+# print(names[-1])
+# print(names[2:4])
+# print(names[2:])
+# print(names[:4])
+# names[4] = "Yup"
+# print(names)
+
+# EXERCISE: Find the largest number in the list
+
+# numbers = [1,2,3,4,5,6,7,8,9]
+
+# max = numbers[0]
+# for number in numbers:
+#     if number > max:
+#         max = number
+# print(max)
