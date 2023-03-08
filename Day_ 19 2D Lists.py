aa423bf5e09f4919df3e3ea3e701783d88ba2f14
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-03-08 20:43:30 +0500

    2D Lists

---
+++ Day_ 19 2D Lists.py
@@ -0,0 +1,18 @@
+# Day_ 19 2D Lists in Python .........................................................................................
+
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+
+# print(matrix[0][1])
+
+# # changing values
+# matrix[0][1] = 20
+# print(matrix[0][1])
+
+# # chaging all values of all the indexes
+# for row in matrix:
+#     for item in row:
+#         print(item)
