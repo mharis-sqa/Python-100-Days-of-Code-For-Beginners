752ebe32198c69622bdc034a406b9b19188ad6e7
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-03-09 21:18:06 +0500

    Add files via upload

---
+++ Day_ 20 List Method.py
@@ -0,0 +1,37 @@
+# Day_ 20 List Method in Python .........................................................................................
+
+# num = [1,2,5,3,5]
+# num.append(55)
+# print(num)
+
+# num.insert(0,10)
+# print(num)
+
+# num.remove(2)
+# print(num)
+
+# num.pop()
+# print(num)
+
+# print(num.count(5))
+
+# num.sort()
+# print(num)
+
+# num2 = num.copy()
+# num.append(6)
+# print(num2)
+
+# print(num.index(3))
+
+# num.clear()
+# print(num)
+
+# EXERCISE: write a program to remove the duplicate in a list
+
+# nums = [1,2,3,55,6,9,3,6,3]
+# unique = []
+# for num in nums:
+#     if num not in unique:
+#         unique.append(num)
+# print(unique)
