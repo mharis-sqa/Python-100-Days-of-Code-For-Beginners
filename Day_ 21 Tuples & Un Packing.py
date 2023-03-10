d11e1f5221eb67abae02e9e709d0ebaa132ef1d5
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-03-10 18:26:45 +0500

    Day_ 21 Tuples / Un Packing

---
+++ Day_ 21 Tuples & Un Packing.py
@@ -0,0 +1,14 @@
+
+# Day_ 21 Tuples / Un Packing in Python .........................................................................................
+
+# you can not modify them its just like lists ... only we can do is count them
+
+## TUPLES:
+# number = (1,3,5)
+# number[0] = 10
+# print(number[0])
+
+## UNPACKING  ## easy way of assgning values
+# coordinates = [1,2,3]
+# x, y, z = coordinates
+# print(x,y,z)
