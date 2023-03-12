d74f703fd94a5048b9bc3ebf04a60aa297c3a716
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-03-12 07:57:13 +0500

    Project Text To Emoji Converter

---
+++ Day_ 23 Text to Emoji Converter.py
@@ -0,0 +1,12 @@
+# Day_ 23 Emoji Converter in Python .........................................................................................
+
+# message = input("> ")
+# words = message.split(' ')
+# emojis = {
+#     ":)": "😀",
+#     ":(": "😥"
+# }
+# output = ""
+# for word in words:
+#     output += emojis.get(word, word) + " "
+# print(output)
