bc13b31269ed7966c9fc5b45503a01cda5fee307
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-03-15 17:05:38 +0500

    Functions

---
+++ Day_ 24 Fuctions.py
@@ -0,0 +1,11 @@
+# Day_ 24 Fuctions in Python .........................................................................................
+
+# always defie your functio before using it :: its a reuseable peice of code
+
+# def greet_user():
+#     print("Hello Haris")
+#     print("How are you")
+
+# print("Start")
+# greet_user()
+# print("Finish") 
