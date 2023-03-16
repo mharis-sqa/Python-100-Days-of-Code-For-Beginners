9a13dc8b3cb88aa2463ba8a5d50986519f243d0a
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-03-16 09:13:41 +0500

    Parameters

---
+++ Day_ 25 Parameters.py
@@ -0,0 +1,32 @@
+# Day_ 25 Parameters in Python .........................................................................................
+
+# # Example 1
+
+# def car_name(name):
+#     print(f"Hi {name}!")
+#     print('Car name is Carola')
+
+# print("Start")
+# car_name("Haris")
+# car_name('Shakir')
+# print("You like it ??")
+
+# # Example 2
+
+# def car_name(f_name, l_name):
+#     print(f"Hi {f_name}, Hi {l_name}!")
+#     print('Car name is Carola')
+
+# print("Start")
+# car_name("Haris", "Shakir")
+# print("You like it ??")
+
+# # Example 3
+
+# def car_name(first_name, last_name):
+#     print(f"Hi {first_name}, Hi {last_name}!")
+#     print('Car name is Carola')
+
+# print("Start")
+# car_name(last_name"Haris", first_name"Shakir")
+# print("You like it ??")
