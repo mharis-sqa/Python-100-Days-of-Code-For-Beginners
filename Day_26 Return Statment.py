dd15597f10142f822e83140bbf77a7c94ceb2667
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-04-01 13:12:44 +0500

    Return Statment

---
+++ Day_26 Return Statment.py
@@ -0,0 +1,14 @@
+# Day_ 26 Return Statment in Python .........................................................................................
+
+# # Example 1
+# def square(number):
+#     return number*number
+
+
+# print(square(3))
+
+# # Example 2
+# def square(number):
+#     print(number*number)
+
+# print(square(3))
