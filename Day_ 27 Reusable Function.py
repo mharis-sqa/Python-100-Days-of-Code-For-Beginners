36576b7e7aba47caab187dedf388ebc36ae95788
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-04-18 21:06:05 +0500

    Reusable Functions

---
+++ Day_ 27 Reusable Function.py
@@ -0,0 +1,15 @@
+# Day_ 27 Creating reusable function in Python .........................................................................................
+
+# def emoji_converter(message):
+#     words = message.split(' ')
+#     emojis = {
+#     ":)": "😀",
+#     ":(": "😥"
+#     }
+#     output = ""
+#     for word in words:
+#         output += emojis.get(word, word) + " "
+#     return output
+
+# message = input("> ")
+# print(emoji_converter(message))
