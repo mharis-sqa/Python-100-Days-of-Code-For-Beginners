63940c475a56abb93c69725ba96fb6c3832f15c0
Muhammad Haris <81009036+MHaris-Dev@users.noreply.github.com>
2023-05-11 09:54:59 +0500

    Classes In Python

---
+++ Day_ 29 Classes.py
@@ -0,0 +1,20 @@
+
+ # Day_ 29 Classes in Python .........................................................................................
+
+# class point:
+#     def move(self):    # function: 1
+#         print("move")
+
+#     def draw(self):
+#         print("draw")  # function: 2
+
+# point1 = point()  # Objects 
+# point1.draw()     # Calling fuction from class here 
+
+
+# point2 = point()
+# point2.x = 10     # Defining attributes we can use attributes in objects
+# point2.y = 20     # we can use attributes in objects
+# print(point2.x)
+# print(point2.y)
+# point2.move()
